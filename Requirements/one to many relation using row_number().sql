a118a731500a1ff43c680c9cdd75ca1640ed23b8
Sreekanth Surampally <34951779+ssurampally@users.noreply.github.com>
2022-07-02 16:20:25 -0600

    Update one to many relation using row_number().sql

--- Requirements/one to many relation using row_number().sql
+++ Requirements/one to many relation using row_number().sql
@@ -18,8 +18,12 @@ select distinct belnr from
        )
       where row_count > 1 ;
       
+
 ----------------------------------------------
 // alternatively you can use count() function
+
+//Actually it is more comfortable to do the duplicate or per count logic with COUNT() instead of row_number()
+
 
 SELECT ITEM_NUMBER, ROW_COUNT FROM 
   (SELECT SEGMENT1 AS ITEM_NUMBER, count(1) AS ROW_COUNT FROM Item_table -- WHERE
@@ -27,3 +31,8 @@ SELECT ITEM_NUMBER, ROW_COUNT FROM
    ) WHERE ROW_COUNT > 1 ;
    
  -----------------------
+// for 2 or more columns -- 
+select vendor_id, address_seq_num, row_count from
+  (select vendor_id, address_seq_num, count(1) as row_count from Vendor_master
+         group by vendor_id, address_seq_num
+   ) where row_count > 1 ;
